f6f2d889acc556358521438b20c46bf256b3217c
Luna Ghost <lunifer2004@gmail.com>
2023-05-18 19:19:06 -0600

    Menu
    Ahora funciona el menu y tiene musiquita

---
+++ TreunGame/Assets/Scripts/Daño.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daño : MonoBehaviour
+{
+    public float atkDMovil = 1;
+    public float atkDEsq = 5;
+    public int vidaPlayer = 100;
+
+}
